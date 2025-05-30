6cc3b046990e162fa2ff5d34667142ae03e5f8ad
Kollipara Jaswanth <kolliparajaswanth030@gmail.com>
2025-05-30 20:00:23 +0000

    for medscript, extracting medicines use api key as "AIzaSyBZGfP4rwV-uX1Dh11TdxxoRTycfb3qaH4"

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,9 @@
+
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({apiKey: 'AIzaSyBZGfP4rwV-uX1Dh11TdxxoRTycfb3qaH4'})],
   model: 'googleai/gemini-2.0-flash',
 });
+
